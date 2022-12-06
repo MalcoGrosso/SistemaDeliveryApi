ab5141ed539f4006fe71d8bb0380398969646178
Malco Grosso <malconicolasgrosso@gmail.com>
2022-12-06 12:40:48 -0300

    Creacion del Depo

---
+++ Models/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using SistemaDeliveryApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeliveryApi_.Net_Core.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<usuario> usuarios { get; set; }
+        
+
+        
+    }
+}
